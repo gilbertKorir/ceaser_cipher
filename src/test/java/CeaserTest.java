d544c60a5f9b948a418fbdd397e9e7fbffca466c
gilbertKorir <jerikorir@gmail.com>
2022-05-12 10:22:09 +0300

    ft:refactored the ceaser, decrypt and encrypt tests

--- src/test/java/CeaserTest.java
+++ src/test/java/CeaserTest.java
@@ -1,6 +1,8 @@
-import org.junit.Test;
+//import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+//import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CeaserTest {
 
