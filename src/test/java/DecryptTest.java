a3c111a51214e0d1647d2cf6a4ccd96da11bdb6d
gilbertKorir <jerikorir@gmail.com>
2022-05-12 11:08:46 +0300

    ft:edited the decrypt test to accomodate the return of string and the entered key

--- src/test/java/DecryptTest.java
+++ src/test/java/DecryptTest.java
@@ -3,6 +3,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DecryptTest {
+    @Test
+    public void newEncrypt_createInstanceOfEncrypt_true() throws Exception{
+        Decrypt decrypt = new Decrypt();
+        String output = decrypt.Decrypt("THE QUICK BROWN", 3);
+        assertEquals(true,output instanceof String);
+    }
 
     @Test
     public void decrypt_decryptMessage_String() throws Exception{
